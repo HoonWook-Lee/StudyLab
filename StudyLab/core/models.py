ef1836749bdc5b2c45c1f5bd6cb868a9d221588f
HoonWook-Lee <gnsdnr1022@naver.com>
2023-06-12 21:42:55 +0900

    유저 DB Modeling

--- StudyLab/core/models.py
+++ StudyLab/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# AbstractUser 사용
+class Users(AbstractUser):
+    # 문자열 최대 50자
+    hint = models.CharField(max_length=50)
