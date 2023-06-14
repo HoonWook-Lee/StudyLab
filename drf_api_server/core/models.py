b2356b20e964826f89562ef18bb2991f4000c895
HoonWook-Lee <gnsdnr1022@naver.com>
2023-06-14 09:18:19 +0900

    메모 DB Modeling

--- drf_api_server/core/models.py
+++ drf_api_server/core/models.py
@@ -26,3 +26,15 @@ class Keywords(TimeStampedModel):
     keyword = models.CharField(max_length=100)
     # 유저 삭제 시 관리자로 변경
     writer = models.ForeignKey(Users, on_delete=models.SET_DEFAULT, default=DEFAULT_ADMIN_PK)
+
+# 메모 모델 생성
+class Memos(TimeStampedModel):
+    title = models.CharField(max_length=100) # 문자열 최대 100자
+    content = models.TextField() # 큰 텍스트 필드 
+    # 유저 삭제 시 관리자로 변경
+    writer = models.ForeignKey(Users, on_delete=models.SET_DEFAULT, default=DEFAULT_ADMIN_PK) 
+    like = models.BigIntegerField(default=0) # 64비트 정수 기본값 = 0
+    # blank = 폼에서 비워둘 수 있음, null = Null 저장 허용
+    img = models.FileField(upload_to='', blank=True, null=True) 
+    # 다대다 관계, Null 허용, 폼 비움 허용
+    keywords = models.ManyToManyField('Keywords', related_name='memos', blank=True)
